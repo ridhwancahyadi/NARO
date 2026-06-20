da14ce64e4de321fdf2e967727ac1493a87724fe
Ridhwan Cahyadi <ridhwancahyadi11@gmail.com>
2026-06-21 00:12:47 +0700

    feat: Connect supabase

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import { BalanceCards } from "./_components/balance-cards";
+
+export const metadata: Metadata = {
+  title: "Fina - Dashboard",
+  description: "Your personal financial dashboard",
+};
+
+export default function DashboardPage() {
+  return (
+    <div className="p-2 space-y-4">
+      <section id="header">
+        <h1 className="text-4xl font-bold text-primary">Dashboard</h1>
+        <p>
+          Get insights into your spending, track your expenses, and manage your
+          finances.
+        </p>
+      </section>
+      <section id="content">
+        <BalanceCards />
+      </section>
+    </div>
+  );
+}
